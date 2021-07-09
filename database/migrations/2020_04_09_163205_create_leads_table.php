d4accb09bfd39453574a6eae6e9d1eba23b03468
dragonkingpro1999 <79324140+dragonkingpro1999@users.noreply.github.com>
2021-07-09 11:45:42 +0700

    Update 2020_04_09_163205_create_leads_table.php

--- database/migrations/2020_04_09_163205_create_leads_table.php
+++ database/migrations/2020_04_09_163205_create_leads_table.php
@@ -16,8 +16,8 @@ class CreateLeadsTable extends Migration
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
+            $table->string('email');
+            $table->string('phone');
             $table->string('picture')->nullable();
             $table->string('address')->nullable();
             $table->string('description')->nullable();
